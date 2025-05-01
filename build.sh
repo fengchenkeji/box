e4007b217a7267d7817200d3ff949918c4f70d0c
fengchenkeji <399233159@qq.com>
2025-05-01 21:00:50 +0800

    描述信息

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf build/*
 cd build
 
 # 安装 Conan 依赖并生成工具链文件
-conan install .. --build=missing --output-folder=. --generator=CMakeDeps --generator=CMakeToolchain -pr=android 
+conan install .. --build=missing --output-folder=. --generator=CMakeDeps --generator=CMakeToolchain -pr=./android 
 
 # 配置 CMake（显式指定工具链）
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release

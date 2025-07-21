89d785bd7129563dfdfc950ef3646b9b41dda3b5
fengchenkeji <399233159@qq.com>
2025-07-21 09:11:17 +0800

    2

--- dow_ndk.sh
+++ dow_ndk.sh
@@ -4,7 +4,8 @@ if [ -f "$PWD/android_ndk" ]; then
 else
     echo "ndk未安装"
     wget -O android.zip https://googledownloads.cn/android/repository/android-ndk-r27d-linux.zip
-    unzip android.zip -d android_ndk
+    unzip android.zip 
+    mv android-ndk-r27d /opt/android_ndk
 fi
 
 # 检查任意类型的文件/目录是否存在

e1ea3f56297a63c2c2fd3549aa4f2cabf15fc463
fengchenkeji <399233159@qq.com>
2025-10-31 16:49:27 +0800

    提交描述（如：第一次提交）

--- build/generators/conanbuild.sh
+++
@@ -1,1 +0,0 @@
-. "/data/data/com.termux/files/home/box/build/generators/conanbuildenv-release-armv7.sh"
--- build/generators/deactivate_conanbuild.sh
+++
@@ -1,1 +0,0 @@
-. "/data/data/com.termux/files/home/box/build/generators/deactivate_conanbuildenv-release-armv7.sh"
